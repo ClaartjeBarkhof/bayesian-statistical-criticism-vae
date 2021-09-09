de6b9538312090917862099f8ba4fb5de85c0bea
ClaartjeBarkhof <claartjebarkhof@hotmail.com>
2021-09-09 13:41:45 +0200

    added arguments, started with objective class

---
+++ arguments.py
@@ -0,0 +1,117 @@
+import distutils
+import configargparse
+
+
+def preprare_parser(print_settings=True):
+    parser = configargparse.ArgParser()
+    parser.add_argument('--config_file', required=False, is_config_file=True, help='config file path')
+
+    # TODO: possibly add AMP, early stopping, load from checkpoint, etc.
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # OBJECTIVE
+    parser.add_argument("--objective", default="VAE", type=str, required=True,
+                        help="Which objective to use, options:"
+                             "  - VAE"
+                             "  - AE"
+                             "  - BETA-VAE, with beta argument set (Higgins et al., 2016)"
+                             "  - FB-VAE (Kingma et al., 2016)"
+                             "  - MDR-VAE (Pelsmaeker & Aziz, 2019)"
+                             "  - INFO-VAE, with alpha and lambda argument set  (Zhao et al., 2017)"
+                             "  - LAG-INFO-VAE (with ...)  (Zhao et al., 2017)")
+    # BETA-VAE
+    parser.add_argument("--beta_beta", default=1.0, type=float,
+                        help="Beta in BETA-VAE objective.")
+
+    # FB-VAE
+    parser.add_argument("--free_bits", default=0.5, type=float, help="The number of Free bits.")
+    parser.add_argument("--free_bits_per_dimension", default=True, type=lambda x: bool(distutils.util.strtobool(x)),
+                        help="Whether or not to apply the Free bits per dimension or with the dimensions combined.")
+
+    # MDR-VAE
+    parser.add_argument("--mdr_value", default=16.0, type=float,
+                        help="The Minimum Desired Rate value (> constraint on rate).")
+
+    # INFO-VAE
+    parser.add_argument("--info_alpha", default=1.0, type=float,
+                        help="The alpha parameter in the INFO-VAE objective (Zhao et al., 2017).")
+    parser.add_argument("--info_lambda", default=1000.0, type=float,
+                        help="The lambda parameter in the INFO-VAE objective (Zhao et al., 2017).")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # BATCHES / TRAIN STEPS
+    parser.add_argument("--batch_size", default=32, type=int,
+                        help="Batch size for data loading and training.")
+    parser.add_argument("--max_global_train_steps", default=1e6, type=int,
+                        help="Maximum number of train steps in total.")
+    parser.add_argument("--max_epochs", default=100, type=int,
+                        help="Maximum number of epochs, if -1 no maximum number of epochs is set.")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # LEARNING RATE
+    parser.add_argument("--lr", default=0.00005, type=float,
+                        help="Learning rate (default: 0.00002).")
+    parser.add_argument("--lr_scheduler", default=False, type=lambda x: bool(distutils.util.strtobool(x)),
+                        help="Whether or not to use a lr scheduler (default: True).")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # LANGUAGE DATASET ARGUMENTS
+    parser.add_argument("--tokenizer_name", default='roberta', type=str,
+                        help="The name of the tokenizer, 'roberta' by default.")
+    parser.add_argument("--language_dataset_name", default='ptb_text_only', type=str,
+                        help="The name of the dataset, 'cnn_dailymail' by default, else: ptb_text_only.")
+    parser.add_argument("--num_workers", default=6, type=int,
+                        help="Num workers for data loading.")
+    parser.add_argument("--max_seq_len", default=64, type=int,
+                        help="What the maximum sequence length the model accepts is (default: 128).")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # TODO: VISION DATASET ARGUMENTS
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # PRINTING & LOGGING
+    parser.add_argument("--print_stats", default=True, type=lambda x: bool(distutils.util.strtobool(x)),
+                        help="Whether or not print stats.")
+    parser.add_argument("--print_every_n_steps", default=10, type=int,
+                        help="Every how many steps to print.")
+    parser.add_argument("--logging", default=True, type=lambda x: bool(distutils.util.strtobool(x)),
+                        help="Whether or not to log the process of the model (default: True).")
+    parser.add_argument("--log_every_n_steps", default=10, type=int,
+                        help="Every how many steps to log (default: 20).")
+    parser.add_argument("--wandb_project", default='fall-2021-VAE', type=str,
+                        help="The name of the W&B project to store runs to.")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # CHECKPOINTING
+    parser.add_argument("--checkpoint", default=True, type=lambda x: bool(distutils.util.strtobool(x)),
+                        help="Whether or not to checkpoint (save) the model. (default: False).")
+    parser.add_argument("--checkpoint_every_n_steps", default=10, type=int,
+                        help="Every how many (training) steps to checkpoint (default: 1000).")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # DISTRIBUTED TRAINING
+    parser.add_argument("--n_gpus", default=1, type=int,
+                        help="Number GPUs to use (default: None).")
+    parser.add_argument("--ddp", default=False, type=lambda x: bool(distutils.util.strtobool(x)),
+                        help="Whether or not to use Distributed Data Parallel (DDP) "
+                             "(default: True if n_gpus > 1, else: False).")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # PARSE & PRINT & RETURN
+
+    args = parser.parse_args()
+
+    if print_settings: print_args(args)
+
+    return parser.parse_args()
+
+def print_args(args):
+    print("-" * 71)
+    print("-" * 30, "ARGUMENTS", "-" * 30)
+    print("-" * 71)
+
+    for k, v in vars(args).items():
+        print(k, ":", v)
+
+    print("-" * 70)
+    print("-" * 70)
--- loss_and_optimisation.py
+++ loss_and_optimisation.py
@@ -1,81 +1,60 @@
 import pytorch_lightning as pl
-from lagrangian_opt import constraint
+from lagrangian_opt.constraint import Constraint, ConstraintOptimizer
 from torch.distributions import kl_divergence
 import torch
 
 
-# TODO: NLL / reconstruction loss / distortion
-
-# TODO: avg. KL divergence / rate
-
-# TODO: MMD
-
-# TODO: total correlation
-
-# class LossTermManager:
-#     def __init__(self, name):
-
-def free_bits_kl(q_z_x, z_post, free_bits=0.0, per_dimension=True):
-    """
-    Calculates the KL-divergence between the posterior and the prior analytically.
-    """
-
-    # [B, D]
-    mus = q_z_x.loc
-    # [B, D, D] -> [B, D] (we assume independent dimensions)
-    logvar = torch.log(q_z_x.covariance_matrix.diagonal(dim1=-1))  # TODO: check this
-
-    kl_loss = 0.5 * (mus.pow(2) + logvar.exp() - logvar - 1)
-    free_bits_kl_loss = None
-
-    if free_bits > 0.0 and per_dimension:
-
-        # Ignore the dimensions of which the KL-div is already under the
-        # threshold, avoiding driving it down even further. Those values do
-        # not have to be replaced by the threshold because that would not mean
-        # anything to the gradient. That's why they are simply removed. This
-        # confused me at first.
-        kl_mask = (kl_loss > free_bits).float()
-
-        # Sum over the latent dimensions and average over the batch dimension
-        free_bits_kl_loss = (kl_mask * kl_loss).sum(-1).mean(0)
-
-    elif free_bits > 0.0 and not per_dimension:
-        # Reduce latent dimensions [B, D] -> [B]
-        kl_loss_red = kl_loss.sum(-1)
-
-        # Mask
-        kl_mask = (kl_loss_red > free_bits).float()
-        free_bits_kl_loss = (kl_mask * kl_loss_red).mean(0)
-
-    kl_loss = kl_loss.sum(-1).mean(0)
-
-    return kl_loss, free_bits_kl_loss
+# TODO: total correlation, dimensionwise KL
+# TODO: Free bits KL for Non-Gaussian case
 
 
 class Objective(pl.LightningModule):
-    def __init__(self, objective, vae):
+    def __init__(self, vae, args):
         super().__init__()
 
         # VAE, FB, MDR,
-        self.objective = objective
+        self.objective = args.objective
         self.vae = vae
         self.prior = self.vae.prior
 
+        self.objective = self.args.objective
+        self.args = args
+
+        # Rate constraint
+        if self.objective == "MDR-VAE":
+            self.mdr_constraint = Constraint(args.mdr_value, ">", alpha=0.5)
+            self.mdr_optimiser = ConstraintOptimizer(torch.optim.RMSprop, self.mdr_constraint.parameters(), 0.00005)
+
     def compute_loss(self, labels, q_z_x, z_post, p_x_z):
         # TODO: implement other types of losses
-
         # Expected KL from prior to posterior
-        rate = self.kl_post_prior(q_z_x)
+        kl_prior_post = self.kl_prior_post(q_z_x)
 
         # Expected negative log likelihood under q
-        distortion = - p_x_z.log_prob(labels).sum(-1).mean(0).item()
+        nll = - p_x_z.log_prob(labels).sum(-1).mean(0).item()
 
-        total_loss = distortion + rate
+        # Maximum mean discrepancy
+        mmd = self.maximum_mean_discrepancy(z_post)
+
+        total_loss = None
+        if self.objective == "AE":
+            total_loss = nll
+        elif self.objective == "VAE":
+            total_loss = nll + kl_prior_post
+        elif self.objective == "BETA-VAE":
+            total_loss = nll + self.args.beta_beta
+        elif self.objective == "MDR-VAE":
+            total_loss = nll + kl_prior_post + self.mdr_constraint(kl_prior_post)
+        elif self.objective == "FB-VAE":
+            total_loss = nll + self.free_bits_kl(q_z_x, z_post, free_bits=self.args.free_bits,
+                                                 per_dimension=self.args.free_bits_per_dimension)
+        elif self.objective == "INFO-VAE":
+            total_loss = - nll + (1 - self.args.info_alpha) * kl_prior_post \
+                         + (self.args.info_alpha + self.args.info_lambda - 1) * mmd
 
         return total_loss
 
-    def kl_post_prior(self, q_z_x, z_post=None, analytical=True):
+    def kl_prior_post(self, q_z_x, z_post=None, analytical=True):
         """
         Computes the KL from prior to posterior, either analytically or empirically,
         depending on whether the posterior distribution is given.
@@ -94,6 +73,44 @@ class Objective(pl.LightningModule):
             kl = log_q_z_x - log_p_z
 
         return kl
+
+    @staticmethod
+    def free_bits_kl(q_z_x, z_post, free_bits=0.0, per_dimension=True):
+        """
+        Calculates the KL-divergence between the posterior and the prior analytically.
+        """
+
+        # [B, D]
+        mus = q_z_x.loc
+        # [B, D, D] -> [B, D] (we assume independent dimensions)
+        logvar = torch.log(q_z_x.covariance_matrix.diagonal(dim1=-1))  # TODO: check this
+
+        kl_loss = 0.5 * (mus.pow(2) + logvar.exp() - logvar - 1)
+        free_bits_kl_loss = None
+
+        if free_bits > 0.0 and per_dimension:
+
+            # Ignore the dimensions of which the KL-div is already under the
+            # threshold, avoiding driving it down even further. Those values do
+            # not have to be replaced by the threshold because that would not mean
+            # anything to the gradient. That's why they are simply removed. This
+            # confused me at first.
+            kl_mask = (kl_loss > free_bits).float()
+
+            # Sum over the latent dimensions and average over the batch dimension
+            free_bits_kl_loss = (kl_mask * kl_loss).sum(-1).mean(0)
+
+        elif free_bits > 0.0 and not per_dimension:
+            # Reduce latent dimensions [B, D] -> [B]
+            kl_loss_red = kl_loss.sum(-1)
+
+            # Mask
+            kl_mask = (kl_loss_red > free_bits).float()
+            free_bits_kl_loss = (kl_mask * kl_loss_red).mean(0)
+
+        kl_loss = kl_loss.sum(-1).mean(0)
+
+        return kl_loss, free_bits_kl_loss
 
     @staticmethod
     def gaussian_kernel(x, y):
