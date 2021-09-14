562d658095da709e8dc9cc66817bc9838756dffa
ClaartjeBarkhof <claartjebarkhof@hotmail.com>
2021-09-14 09:25:29 +0200

    add riannes sylvester flow repo as submodule

---
+++ vae_model/architectures-2.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+import torch
+import numpy as np
+
+
+class EncoderGatedConvolutionBlock(nn.Module):
+    def __init__(self, args):
+        super(EncoderGatedConvolutionBlock, self).__init__()
+
+    forward()
