fixed KL cases

loss_and_optimisation.py
@@ -34,7 +34,7 @@ class Objective(nn.Module):
 
         # TODO: implement other types of losses
         # Expected KL from prior to posterior
-        kl_prior_post = self.kl_prior_post(p_z=p_z, q_z_x=q_z_x, z_post=z_post, analytical=False)
+        kl_prior_post = self.kl_prior_post(p_z=p_z, q_z_x=q_z_x, z_post=z_post, analytical=True)
 
         # Expected negative log likelihood under q
         # TODO: there might be some kind of masking necessary
@@ -71,6 +71,7 @@ class Objective(nn.Module):
 
         return loss_dict
 
+
     def kl_prior_post(self, p_z, q_z_x, z_post=None, analytical=False):
         """
         Computes the KL from prior to posterior, either analytically or empirically,
@@ -80,62 +81,22 @@ class Objective(nn.Module):
         Returns:
         """
 
-        if analytical:
-            # TODO: this is not possible for all
-            # [B] (summed over latent dimension)
-            if isinstance(q_z_x, AutoRegressiveGaussianDistribution):
-                scale = q_z_x.logvars_inferred.mul(0.5).exp_()
-                q_z_x_dist = td.Independent(td.Normal(loc=q_z_x.mus_inferred, scale=scale), 1)
-            else:
-                q_z_x_dist = q_z_x
+        # A bit of a hack to avoid this kl that raises a NotImplementedError
+        if isinstance(p_z, td.MixtureSameFamily):
+            analytical = False
 
-            kl = kl_divergence(q_z_x_dist, p_z).mean()
+        if analytical:
+
+            kl = kl_divergence(q_z_x, p_z).mean()
 
         else:
-            # [1, B]
+            # [B]
             log_q_z_x = q_z_x.log_prob(z_post)
             log_p_z = p_z.log_prob(z_post)
 
             kl = (log_q_z_x - log_p_z).mean()
+
         return kl
-
-    @staticmethod
-    def free_bits_kl(q_z_x, z_post, free_bits=0.0, per_dimension=True):
-        """
-        Calculates the KL-divergence between the posterior and the prior analytically.
-        """
-
-        # [B, D]
-        mus = q_z_x.loc
-        # [B, D, D] -> [B, D] (we assume independent dimensions)
-        logvar = torch.log(q_z_x.covariance_matrix.diagonal(dim1=-1))  # TODO: check this
-
-        kl_loss = 0.5 * (mus.pow(2) + logvar.exp() - logvar - 1)
-        free_bits_kl_loss = None
-
-        if free_bits > 0.0 and per_dimension:
-
-            # Ignore the dimensions of which the KL-div is already under the
-            # threshold, avoiding driving it down even further. Those values do
-            # not have to be replaced by the threshold because that would not mean
-            # anything to the gradient. That's why they are simply removed. This
-            # confused me at first.
-            kl_mask = (kl_loss > free_bits).float()
-
-            # Sum over the latent dimensions and average over the batch dimension
-            free_bits_kl_loss = (kl_mask * kl_loss).sum(-1).mean(0)
-
-        elif free_bits > 0.0 and not per_dimension:
-            # Reduce latent dimensions [B, D] -> [B]
-            kl_loss_red = kl_loss.sum(-1)
-
-            # Mask
-            kl_mask = (kl_loss_red > free_bits).float()
-            free_bits_kl_loss = (kl_mask * kl_loss_red).mean(0)
-
-        kl_loss = kl_loss.sum(-1).mean(0)
-
-        return kl_loss, free_bits_kl_loss
 
     @staticmethod
     def gaussian_kernel(x, y):

vae_model/inference_model.py
@@ -60,7 +60,7 @@ class InferenceModel(nn.Module):
         q_z_x_params = self.q_z_nn(x_in)
         print("infer_q_z_x q_z_x_params", q_z_x_params.shape)
         q_z_x = self.q_z_x_block(q_z_x_params)
-        print("infer_q_z_x q_z_x", q_z_x)
+        #print("infer_q_z_x q_z_x", q_z_x)
 
         return q_z_x
 
@@ -68,7 +68,7 @@ class InferenceModel(nn.Module):
         """Infers a distribution and samples from it with the reparameterisation trick."""
         # [S, B, D]
         q_z_x = self.infer_q_z_x(x_in)
-        z_post = q_z_x.rsample() # TODO: sample_shape=(n_samples,)
+        z_post = q_z_x.rsample()  # TODO: sample_shape=(n_samples,)
 
         return q_z_x, z_post
 
@@ -82,9 +82,7 @@ class IndependentGaussianBlock(nn.Module):
         self.mean_layer = nn.Linear(256, self.D)
         self.scale_layer = nn.Sequential(nn.Linear(256, self.D), nn.Softplus())
 
-
     def forward(self, q_z_x_params):
-
         mean = self.mean_layer(q_z_x_params)
         scale = self.scale_layer(q_z_x_params)
 
@@ -95,57 +93,120 @@ class IndependentGaussianBlock(nn.Module):
 
 
 class AutoRegressiveGaussianDistribution(nn.Module):
-    def __init__(self, z_sample, made_block=None, mu_inferred=None, scale_inferred=None):
+    def __init__(self, q_z_x_params, made, state=None):
         super(AutoRegressiveGaussianDistribution, self).__init__()
 
-        self.z_sample = z_sample
+        self.q_z_x_params = q_z_x_params
+        self.made = made
+        self.D = self.made.nin
+        self.state = state  # z, mu, scale
 
-        assert made_block is not None or (mu_inferred is not None and scale_inferred is not None), \
-            "Either give a MADE as input or the inferred mus and scales"
-
-        self.made_block = made_block # ConditionalGaussianBlockMADE: careful this is more than the MADE itself
-        self.mu_inferred = mu_inferred
-        self.scale_inferred = scale_inferred
-
-    def log_prob(self, z):
-        if self.mu_inferred is None or self.scale_inferred is None:
-            output_made = self.made_block(z)
+    def log_prob(self, z, mean_scale=None):
+        if mean_scale is not None:
+            mean, scale = mean_scale
+        elif self.state is not None:
+            mean, scale = self.state[1], self.state[2]
+        else:
+            output_made = self.made(z, context=self.q_z_x_params)
             params_split = torch.split(output_made, 2, dim=1)
             mean, pre_scale = params_split[0], params_split[1]
             scale = F.softplus(pre_scale)
-        else:
-            mean, scale = self.mus_inferred, self.scale_inferred
 
+        self.state = (z, mean, scale)
+
+        # careful that the distribution itself is not independent, but at this point it is valid to use for log_prob
         log_q_z_x = td.Independent(td.Normal(loc=mean, scale=scale), 1).log_prob(z)
+
         return log_q_z_x
 
     def rsample(self):
-        return self.z_sample
+        # TODO: multi sample forward?
+        B = self.q_z_x_params.shape[0]
+
+        z_sample = torch.zeros((B, self.D))
+        mu_inferred, scale_inferred = [], []
+
+        for i in range(self.D):
+            mus_prescales = self.made(z_sample, context=self.q_z_x_params)
+            # split in 2 x [B, D]
+            mus_prescales = torch.split(mus_prescales, 2, dim=1)
+            mus, prescales = mus_prescales[0], mus_prescales[1]
+
+            mu_i = mus[:, i]
+            scale_i = F.softplus(prescales[:, i])
+
+            mu_inferred.append(mu_i)
+            scale_inferred.append(scale_i)
+
+            z_sample[:, i] = td.Normal(loc=mu_i, scale=scale_i).rsample()
+
+        mu_inferred = torch.stack(mu_inferred, dim=1)
+        scale_inferred = torch.stack(scale_inferred, dim=1)
+
+        self.state = (z_sample, mu_inferred, scale_inferred)
+
+        return z_sample
+
+
+@td.register_kl(AutoRegressiveGaussianDistribution, AutoRegressiveGaussianDistribution)
+def _kl(p, q):
+    if p.state is None:
+        _ = p.rsample()
+    p_mean, p_scale = p.state[1], p.state[2]
+
+    if q.state is None:
+        _ = q.rsample()
+    q_mean, q_scale = q.state[1], q.state[2]
+
+    kl = td.kl_divergence(td.Normal(loc=p_mean, scale=p_scale), td.Normal(loc=q_mean, scale=q_scale)).sum(-1)
+
+    return kl
+
+
+@td.register_kl(AutoRegressiveGaussianDistribution, td.Normal)
+def _kl(p, q):
+    if p.state is None:
+        _ = p.rsample()
+    p_mean, p_scale = p.state[1], p.state[2]
+
+    kl = td.kl_divergence(td.Normal(loc=p_mean, scale=p_scale), q).sum(-1)
+
+    return kl
+
+
+@td.register_kl(AutoRegressiveGaussianDistribution, td.Distribution)
+def _kl(p, q):
+    if p.state is None:
+        z = p.rsample()
+    else:
+        z = p.state[0]
+
+    log_p_z = p.log_prob(z)
+    log_q_z = q.log_prob(z)
+
+    kl = log_p_z - log_q_z
+
+    return kl
+
 
 class ConditionalGaussianBlockMADE(nn.Module):
     def __init__(self, args):
         super(ConditionalGaussianBlockMADE, self).__init__()
 
         self.D = args.latent_dim
-        #self.mapping_layer = nn.Linear(256, self.D)
+        # self.mapping_layer = nn.Linear(256, self.D)
 
         hiddens = [200, 220]
 
         natural_ordering = True
         act = nn.ReLU()
 
-        self.made_block = MADE(self.D, hiddens, int(self.D * 2), natural_ordering=natural_ordering,
-                               context_size=256, hidden_activation=act)  # no additional context here
+        self.made = MADE(self.D, hiddens, int(self.D * 2), natural_ordering=natural_ordering,
+                         context_size=256, hidden_activation=act)  # no additional context here
 
     def forward(self, q_z_x_params):
-        #made_initial_input = self.mapping_layer(q_z_x_params)
-
-        # All [B, D]
-        z_sample, mu_inferred, scale_inferred = self.made_block.auto_regressive_sampling(q_z_x_params)
-
         # Placeholder distribution object
-        q_z_x = AutoRegressiveGaussianDistribution(z_sample=z_sample, mu_inferred=mu_inferred,
-                                                   scale_inferred=scale_inferred)
+        q_z_x = AutoRegressiveGaussianDistribution(q_z_x_params=q_z_x_params, made=self.made)
 
         return q_z_x
 